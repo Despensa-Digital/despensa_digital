e94686b4d844d3c133edce9377fe6882d3a0f731
Luna <giulianna.lancellotti@gmail.com>
2023-09-24 14:47:11 -0300

    [Chore]: Configurando ambiente para a máquina da Luna

--- android/app/src/main/java/com/despensadigital/MainActivity.java
+++ android/app/src/main/java/com/despensadigital/MainActivity.java
@@ -1,11 +1,11 @@
 package com.despensadigital;
-import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
-
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -31,7 +31,7 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
-
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
