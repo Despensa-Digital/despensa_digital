a6f83010f91352ee94412d4f5ee6eefec1db254a
joel <joelvictor1746@gmail.com>
2023-10-18 13:42:15 -0300

    chore: ajusta fluxo da tela de listar

--- View/HomeTabs/Listas/ListaMercado.js
+++ View/HomeTabs/Listas/ListaMercado.js
@@ -11,20 +11,23 @@ const ListaMercado = () => {
       {
         "title": "Arroz 1kg",
         "description": "R$4,99",
+        "valor": 4.99,
         "image": require('../../../Assets/Products/Rice.png'),
-        "counter": 0
+        "counter": 1
       },
       {
         "title": "Cerveja",
         "description": "R$6,99",
+        "valor": 6.99,
         "image": require('../../../Assets/Products/beer.png'),
-        "counter": 0
+        "counter": 1
       },
       {
         "title": "Leite",
         "description": "R$7,99",
+        "valor": 7.99,
         "image": require('../../../Assets/Products/Milk.png'),
-        "counter": 0
+        "counter": 1
       },
     ]
   )
@@ -33,14 +36,50 @@ const ListaMercado = () => {
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
   
+  const limparLista = () => {
+    setItens([]);
+    recalculateTotal();
+  }
+
+  const mockIncrement = (index, direction) => {
+    let newItens = [...itens];
+
+    if (direction == "minus" ) {
+      newItens[index].counter--;
+      if (newItens[index].counter <= 0) {
+        newItens.pop(index)
+      }
+    }else {
+      newItens[index].counter++;
+    }
+
+    setItens(newItens);
+    recalculateTotal();
+  }
+  
 
   const [nome, setNome] = useState('');
   const [valor, setValor] = useState(0);
 
+  const [total, setTotal] = useState(0);
+  
+  const recalculateTotal = () => {
+    let novoTotal = 0;
+    for (i = 0; i < itens.length; i++) {
+      novoTotal += (itens[i].counter * itens[i].valor);
+    }  
+    novoTotal = novoTotal.toFixed(2);
+    setTotal(novoTotal);
+  }
+  
+  useEffect(() => {
+    recalculateTotal();
+  });
+
   return (
   <PaperProvider>
    
-   {itens.map((item) =>
+   {itens.map((item, index) =>
     <List.Item
       title={item.title}
       description={item.description}
@@ -48,21 +87,21 @@ const ListaMercado = () => {
       left={props => <Image source={item.image} style={{ width: 50, height: 50 }} />} 
       right={props => 
       <View style={{flexDirection:"row"}}>
-        <IconButton {...props} style={{borderRadius:10,marginRight:10, backgroundColor: '#ffb3b3'}} icon="minus" onPress={() => console.log("menos")} />
+        <IconButton {...props} style={{borderRadius:10,marginRight:10, backgroundColor: '#ffb3b3'}} icon="minus" onPress={() => mockIncrement(index, "minus")} />
         <Text style={{alignSelf:"center", position:"absolute", left: "47%"}}>{item.counter}</Text>
-        <IconButton {...props} style={{borderRadius:10, marginLeft:30, backgroundColor: '#b0ea93'}} icon="plus" onPress={() => console.log("mais")} />
+        <IconButton {...props} style={{borderRadius:10, marginLeft:30, backgroundColor: '#b0ea93'}} icon="plus" onPress={() => mockIncrement(index, "plus")} />
       </View>
     }
       // each item should have a plus and a minus button as if it was a counter on the right side of the item, they should be at the bottom of the item and should be clickable
     />
    )}
-        <Text style={{alignSelf:"center",marginTop:10, color:"#a2a2a2"}}>Limpar lista</Text>
+        <Text onPress={limparLista} style={{alignSelf:"center",marginTop:10, color:"#a2a2a2"}}>Limpar lista</Text>
         {/* the sum of all prices should be a card just like the itens above but with only centralized text*/}
 
         <Text style={{fontSize:10, alignSelf:"left",margin:20, marginBottom: 0, color:"#a2a2a2"}}>Valor acumulado</Text>
 
         <View style={{margin: 10, marginTop: 0, padding: 20, borderRadius:15,  backgroundColor:"#f2f2f2"}}>
-          <Text style={{alignSelf:"center"}}>R$ 19,97</Text>
+          <Text style={{alignSelf:"center"}}>R$ {total}</Text>
         </View>
 
         <Portal>
